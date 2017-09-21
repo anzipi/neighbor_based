2a7e10429d94060eb12dfbe52099f910a2f73355
crazyzlj <crazyzlj@gmail.com>
2017-09-21 11:04:42 +0800

    refactor code structure

---
+++ src/common_func.h
@@ -0,0 +1,16 @@
+﻿#ifndef H_COMMON_FUNC
+#define H_COMMON_FUNC
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace::std;
+#define VERY_SMALL 0.000001
+
+double string_to_double( const std::string& s );
+void parseStr(string str, char c, vector<string>& tokens);
+string& trimLineBreak(string &s);
+int getMin(int a, int b);
+double getSimilaityIntegration(double * similarity, int numOfLyr, double noData);
+double getMaxValue(double* similarity, int length, double noData);
+
+#endif /* H_COMMON_FUNC */
